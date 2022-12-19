d85d149a2e9c5318059736b61703f12c85e2e1e5
DavideBertocco11 <davide.bertocco@alessandrinimainardi.edu.it>
2022-12-19 08:24:06 +0100

    salva

--- news/views.py
+++ news/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
-
+#from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Articolo, Giornalista
 # Create your views here.
+def home(request):
+    #a = ""
+    #g = ""
+
+    #a = []
+    #g = []
+
+    #for art in Articolo.objects.all():
+        #a += (art.titolo + "<br>")
+
+        #a.append(art.titolo)
+
+    #for gio in Giornalista.objects.all():
+        #g += (gio.nome + "<br>")
+
+        #g.append(gio.nome)
+
+    articoli = Articolo.objects.all()
+    giornalisti = Giornalista.objects.all()
+    context = {"articoli": articoli, "giornalisti": giornalisti}
+    print(context)
+    return render(request, "homepage.html", context)
+
+    #response = "Articoli:<br>" + a + "<br>Giornalisti<br>" + g
+
+    #response = str(a) + "<br>" + str(g)
+    #print(response)
+
+    #return HttpResponse("<h1>" + response + "</h1>")
+
+def articoloDetailView(request, pk):
+    articolo = get_object_or_404(Articolo, pk=pk)
+    context = {"articolo": articolo}
+    return render(request, "articolo_detail.html", context)
--- primo_progetto/settings.py
+++ primo_progetto/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'prima_app',
     'seconda_app',
     'templates',
-    'prova_pratica_0'
+    'prova_pratica_0',
+    'news'
 ]
 
 MIDDLEWARE = [
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'primo_progetto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'prima_app/templates'), os.path.join(BASE_DIR, 'seconda_app/templates'), os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'prova_pratica_0/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'prima_app/templates'), os.path.join(BASE_DIR, 'seconda_app/templates'), os.path.join(BASE_DIR, 'news/templates'), os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'prova_pratica_0/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
